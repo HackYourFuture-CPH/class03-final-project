0e365b406fd517455d2b24021dd6c22b96445835
Jacob Andresen <jacob.andresen@gmail.com>
2018-05-12 17:21:21 +0200

    create static index file with api description

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var categoriesRouter = require('./routes/categories');
 
 var app = express();
@@ -10,9 +9,8 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
-
-app.use('/', indexRouter);
 
 // TODO: insert a new route here :)
 app.use('/categories', categoriesRouter);
