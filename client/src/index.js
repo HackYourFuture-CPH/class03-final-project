9fc1a2c154396bfc11fa966f2d5a980bcc2266db
Asma <rababhina@hotmail.com>
2018-05-20 13:39:59 +0200

    [iteration 3]

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import {BrowserRouter} from 'react-router-dom';
 import Contentform from './components/Contentform';
-import { Motion, spring } from "react-motion";
+
 
 
 import registerServiceWorker from './registerServiceWorker';
