d7fd2cc2f8fbdeab15e3ec8dd01952b13606e75c
Osilamah <soloosilamah@yhaoo.com>
2018-05-20 13:31:14 +0200

    ratings

---
+++ client/src/components/ratings.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
+class Ratings extends Component {
+
+}
+export default Ratings;
